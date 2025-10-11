d455dec27a02a2d6a7b8448d32bcb19839a9ab57
m_amini <mr.amini.env@gmail.com>
2025-10-11 14:27:27 +0330

    part -3 configuration

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -2,7 +2,7 @@
   <header class="w-full text-sm">
     <div class="fixed top-0 left-0 w-full h-16 bg-white">
       <div class="flex flex-nowrap h-full border-b border-solid border-brand-gray-1 px-8 mx-auto">
-        <a href="#" class="flex h-full items-center text-xl">MR Careers </a>
+        <a href="#" class="flex h-full items-center text-xl">{{ company }} </a>
       </div>
     </div>
   </header>
@@ -11,5 +11,10 @@
 <script>
 export default {
   name: 'MainNav',
-}
+  data(){
+    return{
+      company: "MR Careers",
+    };
+  }
+};
 </script>
