75d2c550ad0aea7e56b10cfd95468d3dc6a3f74b
m_amini <mr.amini.env@gmail.com>
2025-11-11 16:40:40 +0330

    feat: add main Headline component to core layout

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -1,5 +1,5 @@
 <template>
-  <header class="w-full text-sm">
+  <header :class="['w-full', 'text-sm', headerHeightClass]">
     <div class="fixed top-0 left-0 w-full h-16 bg-white">
       <div class="flex flex-nowrap h-full border-b border-solid border-brand-gray-1 px-8 mx-auto">
         <a :href="url" class="flex h-full items-center text-xl">{{ company }} </a>
@@ -12,7 +12,7 @@
         </nav>
         <div class="ml-auto flex h-full items-center py-2.5">
           <profile-image v-if="isLoggedIn" />
-          <action-button v-else @click="toggleLogged" text="Sign in" type="secondary " />
+          <action-button v-else @click="toggleLogged" text="Sign in" type="primary" />
         </div>
       </div>
       <the-subnav v-if="isLoggedIn" />
@@ -38,6 +38,14 @@ export default {
       menuItems: ['Teams', 'Location', 'Life at MR Corp', 'How We Hire', 'Students', 'jobs'],
       isLoggedIn: false,
     }
+  },
+  computed: {
+    headerHeightClass() {
+      return {
+        'h-16': !this.isLoggedIn,
+        'h-32': this.isLoggedIn,
+      }
+    },
   },
   methods: {
     toggleLogged() {
