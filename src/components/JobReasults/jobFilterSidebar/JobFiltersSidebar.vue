4a24ff81ef31aba818f65ac3695d419e5a7c46a8
m_amini <mr.amini.env@gmail.com>
2026-02-18 11:13:37 +0330

    feat(filters-sidebar): add initial structure and base styling for jobs list filters

--- src/components/JobReasults/jobFilterSidebar/JobFiltersSidebar.vue
+++ src/components/JobReasults/jobFilterSidebar/JobFiltersSidebar.vue
@@ -1,9 +1,21 @@
 <template>
-  <div class="flex flex-col border-r border-solid border-brand-gray-1 bg-white p-4 w-96">Job Filter Sidebar</div>
+  <div class="flex flex-col border-r border-solid border-brand-gray-1 bg-white p-4 w-96">
+    <section class="pb-5">
+      <div class="flex flex-row justify-between">
+        <h3 class="my-4 text-base font-semibold">What do you want to do?</h3>
+        <div class="flex items-center text-sm ">
+          <ActionButton text="Clear Filters" type="secondary" />
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
+import ActionButton from '@/components/Shared/ActionButton.vue';
+
 export default {
   name: 'JobFiltersSidebar',
+  components: { ActionButton },
 }
 </script>
