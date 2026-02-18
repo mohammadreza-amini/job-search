c9a8cc879002c9b976c902efbebd1e09ef34a42b
m_amini <mr.amini.env@gmail.com>
2026-02-18 16:34:43 +0330

    feat(components): create CollapsibleAccordion with slot-based content

--- src/components/JobReasults/jobFilterSidebar/JobFiltersSidebar.vue
+++ src/components/JobReasults/jobFilterSidebar/JobFiltersSidebar.vue
@@ -3,19 +3,21 @@
     <section class="pb-5">
       <div class="flex flex-row justify-between">
         <h3 class="my-4 text-base font-semibold">What do you want to do?</h3>
-        <div class="flex items-center text-sm ">
-          <ActionButton text="Clear Filters" type="secondary" />
+        <div class="flex items-center text-sm">
+          <action-button text="Clear Filters" type="secondary" />
         </div>
       </div>
+      <collapsible-accordion>Anything we want</collapsible-accordion>
     </section>
   </div>
 </template>
 
 <script>
-import ActionButton from '@/components/Shared/ActionButton.vue';
+import ActionButton from '@/components/Shared/ActionButton.vue'
+import CollapsibleAccordion from '@/components/Shared/CollapsibleAccordion.vue';
 
 export default {
   name: 'JobFiltersSidebar',
-  components: { ActionButton },
+  components: { ActionButton,CollapsibleAccordion },
 }
 </script>
